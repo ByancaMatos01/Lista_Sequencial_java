2c5cb22d140805079f27946cbb30c524b06d06f8
Byanca Matos <122841376+ByancaMatos01@users.noreply.github.com>
2024-01-08 14:41:41 -0300

    Update exercicio1.java

--- sequencial/exercicio1.java
+++ sequencial/exercicio1.java
@@ -1,7 +1,14 @@
 package sequencial;
 
 import java.util.Scanner;
+/*
+ * Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma
+mensagem explicativa, conforme exemplos.
+ */
 
+/*
+ * Descricão num1= numero1, num2= numero2, result= resultado
+ */
 public class exercicio1 {
 public static void main(String args[]) {
 	Scanner leia=new Scanner(System.in);
